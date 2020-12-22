df688eb9c26f7f53bc8b1cd6a44ed4ffe9b8fafa
gauravdwivedi <gauravdwivedi@hotmail.com>
2020-12-22 16:01:36 +0530

    scaffolding

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers} from 'redux'
+import postReducers from './postReducers'
 
 
-
-export default combineReducers({
-    initialState:()=>'Beginning'
+export default combineReducers()({
+    posts:postReducers    
 })
