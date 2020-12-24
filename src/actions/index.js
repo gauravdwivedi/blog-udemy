bcc43c374f0312b027c04aa38e3e55127655aeae
gauravdwivedi <gauravdwivedi@hotmail.com>
2020-12-24 17:27:42 +0530

    one time memoization

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import jsonplaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = ()=> async dispatch=>{
@@ -12,10 +13,17 @@ export const fetchPosts = ()=> async dispatch=>{
 
 
   export const fetchUser =(id)=> async dispatch=>{
-            const response = await jsonplaceholder.get(`/users/${id}`);
-
-            dispatch({
-                type:'FETCH_USER',
-                payload:response.data
-            })
+    _fetchUser(id,dispatch)  
   }
+
+//private function
+  const _fetchUser = _.memoize(async (id,dispatch) =>{
+    const response = await jsonplaceholder.get(`/users/${id}`);
+
+    dispatch({
+        type:'FETCH_USER',
+        payload:response.data
+    })
+  })
+
+
