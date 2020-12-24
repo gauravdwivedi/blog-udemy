accd82d46c77ec87f8a28b6dd2bf348dab24f78a
gauravdwivedi <gauravdwivedi@hotmail.com>
2020-12-24 15:57:58 +0530

    getting userHeader

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers} from 'redux'
 import postReducers from './postReducers'
+import usersReducers from './usersReducers'
 
 
 export default combineReducers({
-    posts:postReducers    
+    posts:postReducers,   
+    users:usersReducers
 })
 
  
